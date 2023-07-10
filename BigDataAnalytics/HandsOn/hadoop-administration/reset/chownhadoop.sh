194a448478f3a5ec8a6b5b44df2849ba4269c8a5
Jigar Pandya <jigarpandya.ce@gmail.com>
2023-07-10 16:36:18 +0530

    Hadoop administration and more.

---
+++ BigDataAnalytics/HandsOn/hadoop-administration/reset/chownhadoop.sh
@@ -0,0 +1,35 @@
+echo "Usage: mycmdname subnet iprangestart iprangeend username"
+if [[ -z $1 ]]
+then
+	echo "Enter subnet"
+	read subnet
+	echo "Enter iprangestart (only computer node identifier)"
+	read iprangestart
+	echo "Enter iprangeend (only computer node identifier)" 
+	read iprangeend
+	echo "Enter username"
+	read username
+else
+	subnet=$1
+	iprangestart=$2
+	iprangeend=$3
+	username=$4
+fi
+
+echo "Enter password"
+read -s password
+echo "Result"
+ export SSHPASS="$password"
+
+for ipn in $(eval echo "{$iprangestart..$iprangeend}");
+do
+	if test $ipn -eq 31 -o $ipn -eq 52 -o $ipn -eq 53 -o $ipn -eq 54
+	then
+		continue
+	fi
+	echo "Processing on $subnet.$ipn"
+	sshpass -e ssh -o StrictHostKeyChecking=no $username@$subnet.$ipn "chown -R hadoop:hadoop /opt/hadoop-3.3.3"
+	echo
+done
+
+echo "Complete"
---
+++ BigDataAnalytics/HandsOn/hadoop-administration/setup/demo_psuedo_sbin/start-historyserver.sh
@@ -0,0 +1,2 @@
+#mr-jobhistory-daemon.sh --config $HADOOP_CONF_DIR start historyserver
+mapred --daemon start historyserver
