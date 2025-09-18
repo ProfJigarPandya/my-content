474cae7a8387e860908303e738afa4ec177e2151
ProfJigarPandya <jigarpandya.ce@gmail.com>
2025-09-18 14:05:39 +0530

    Application to run client vs cluster; Resource Manager can be local, yarn or kubernetes

--- BigDataAnalytics/Practice/spark-4.0.1-docker/standalone_mode/spark-docker-start-worker1.sh
+++
@@ -1,6 +0,0 @@
-docker run -d --user spark \
---env SPARK_HOME=/opt/spark \
---name apache-spark-r-w1 \
---hostname spark-worker1 \
--p 4040:4040 -p 8080:8080 -p 7077:7077 -p 8081:8081 -p 18080:18080 \
--it apache/spark:r bash
