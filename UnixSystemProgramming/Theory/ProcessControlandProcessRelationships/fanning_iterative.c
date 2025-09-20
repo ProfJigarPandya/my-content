92e5cec9fff4fd6d7d3c3f8e25dbacc016ac0e9a
ProfJigarPandya <jigarpandya.ce@gmail.com>
2025-09-20 20:43:58 +0530

    fanning iterative

---
+++ UnixSystemProgramming/Theory/ProcessControlandProcessRelationships/fanning_iterative.c
@@ -0,0 +1,93 @@
+/* Fanning of processes. 
+ Iterative version
+ Can verify from process id and parent process id;
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[])
+{
+	int total_chain_nodes=-1, existing_chain_nodes=1;
+
+	if(argc<2)//Program pathname and a number 
+	{
+		printf("\n Usage: Program <number of nodes>");
+		exit(EXIT_FAILURE);
+	}
+	if(existing_chain_nodes==1)//First node first parent itself
+	{
+		total_chain_nodes = atoi(argv[1]);
+		printf("\n Total Chain nodes expected %d\n", total_chain_nodes);
+		if(total_chain_nodes <=0)
+		{
+			printf("\n Parent is counted minimum one node");
+			exit(EXIT_FAILURE);
+		}
+		printf("getpid()\tgetppid()\texisting_chain_nodes\ttotal_chain_nodes\n");
+		printf("\n PARENT: (%d\t%d\t%d\t%d)",getpid(),getppid(),existing_chain_nodes,total_chain_nodes);
+		fflush(stdout);
+	}
+	
+	if(existing_chain_nodes==total_chain_nodes)
+	{
+		printf("\n At terminating condition!");
+		fflush(stdout);
+		return(EXIT_SUCCESS);
+	}
+	else
+	{
+		int wstatus;
+		pid_t cpid[total_chain_nodes],w;
+		for(int t=2;t<=total_chain_nodes;t++)
+		{
+			cpid[t]=fork();
+			if(cpid[t]==-1)
+			{
+				perror("\n Fork error ");
+				exit(EXIT_FAILURE);
+			}
+			if(cpid[t]!=0)//parent
+			{
+				existing_chain_nodes++;
+				continue;
+			}
+			else//child
+			{
+				printf("\n CHILD: (%d\t%d\t%d\t%d)\n",getpid(),getppid(),existing_chain_nodes,total_chain_nodes);
+				fflush(stdout);
+				exit(EXIT_SUCCESS);
+			}
+		}//end of looping through child status change
+			
+		for(int t=2;t<=total_chain_nodes;t++)
+		{
+				//parent to collect status of chile.
+				do {
+                   			w = waitpid(cpid[t], &wstatus, WUNTRACED | WCONTINUED);
+                   			if (w == -1) {
+                       				perror("waitpid");
+                       				exit(EXIT_FAILURE);
+                   			}
+	
+        	           		if (WIFEXITED(wstatus)) {
+                	       			printf("exited, status=%d\n", WEXITSTATUS(wstatus));
+                   			} else if (WIFSIGNALED(wstatus)) {
+                       				printf("killed by signal %d\n", WTERMSIG(wstatus));
+                   			} else if (WIFSTOPPED(wstatus)) {
+                       				printf("stopped by signal %d\n", WSTOPSIG(wstatus));
+                  			} else if (WIFCONTINUED(wstatus)) {
+                       				printf("continued\n");
+                 	  		}
+               			} while (!WIFEXITED(wstatus) && !WIFSIGNALED(wstatus));
+				printf("\n Reaped %d \n",cpid[t]);
+				fflush(stdout);
+		}
+	}//end of else
+	printf("\n At end of main \n");
+	fflush(stdout);
+	return (EXIT_SUCCESS);
+
+}
