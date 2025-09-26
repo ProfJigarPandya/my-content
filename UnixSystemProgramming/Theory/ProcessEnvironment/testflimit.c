3ea5a1708d6af9fe5f752366a8e71007ad989ae2
ProfJigarPandya <jigarpandya.ce@gmail.com>
2025-09-26 12:02:21 +0530

    information about system and process limits

--- UnixSystemProgramming/Theory/ProcessEnvironment/testflimit.c
+++ UnixSystemProgramming/Theory/ProcessEnvironment/testflimit.c
@@ -4,9 +4,38 @@
  * for i in {1..6000}; do echo "$i" >> $i; done;
  * cat /proc/`pgrep a.out`/limits
  * prlimit --pid xxxxx --nofile=3024:4095
+
+   for i in {1..6000}; do rm -rf $i; done;
+
+
  */
 
+/*
+man ulimit
+
+ A hard limit cannot be  increased
+              by  a non-root user once it is set; a soft limit may be increased up to the value of
+              the hard limit.
+
+       struct rlimit {
+           rlim_t  rlim_cur;  /* Soft limit */
+           rlim_t  rlim_max;  /* Hard limit (ceiling for rlim_cur) */
+       };
+
+       typedef /* ... */  rlim_t;  /* Unsigned integer type */
+
+
+
+man prlimit
+prlimit - get and set process resource limits
+
+
+
+*/
+
+
 #include <stdio.h>
+#include <stddef.h>
 #include <errno.h>
 #include <string.h>
 
