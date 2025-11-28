4f8e4b857acfd6b3717622b8277546d01d5741d0
ProfJigarPandya <jigarpandya.ce@gmail.com>
2025-11-28 14:38:43 +0530

    sig fpe raise

---
+++ UnixSystemProgramming/Theory/Signals/sigfpe.c
@@ -0,0 +1,27 @@
+void sig_fpe(int signo)
+{
+	if (signo == SIGFPE) {
+		printf("\nSignal SIGFPE received.\n");
+		exit(1);
+	}
+}
+int gen_fpe() {
+	int x = 0 ;
+	return 1 % x ;
+}
+int main ( ) 
+{
+	struct sigaction sa; 
+	
+	sa.sa_handler = sig_fpe;
+	sa.sa_flags = 0 ;
+	sigemptyset(&sa.sa_mask) ;
+	
+	sigaction(SIGFPE, &sa , NULL) ;
+
+	//gen_fpe() ;
+	//raise(SIGFPE);//8
+	printf("\nGoodbye!!\n");
+	return ( 0 ) ;
+}
+
